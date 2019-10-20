4d080f4693bb9969802257ba44c95191c0141ad8
AJINKYA BALASAHEB DESHMUKH <ajinkyadeshmukh9@gmail.com>
2019-10-20 10:49:24 +0530

    updated ci database key

--- config/ci.js
+++ config/ci.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://blogger:QqiLz8WFRVjtuwQK@cluster0-shard-00-00-9vwri.mongodb.net:27017,cluster0-shard-00-01-9vwri.mongodb.net:27017,cluster0-shard-00-02-9vwri.mongodb.net:27017/Blockchain_wallet?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    mongoURI: 'mongodb://127.0.0.1:27017/Blockchain_wallet',
     cookieKey: '123123123',
     redisUrl : 'redis://127.0.0.1:6379',
     port: '3000'
