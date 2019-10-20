7a07cf8de2815d99d08268e9a7ebb62e07b01771
AJINKYA BALASAHEB DESHMUKH <ajinkyadeshmukh9@gmail.com>
2019-10-20 16:35:19 +0530

    modified ci redis key

--- config/ci.js
+++ config/ci.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongoURI: 'mongodb://127.0.0.1:27017/Blockchain_wallet',
     cookieKey: '123123123',
-    redisUrl : 'redis://redis:6379',
+    redisUrl : 'redis://127.0.0.1:6379',
     port: '3000'
   };
   
